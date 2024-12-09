0b88d78416429bfd6b84baccd8184f3ffbe92ddc
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-12-09 10:47:10 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,24 +1,14 @@
 (async c=> (
   c.declarativeNetRequest.updateEnabledRulesets(
-    (await c.storage.local.get("0"))[0] ? (
-      c.action.setIcon({path: "off.png"}),
-      {disableRulesetIds:["0"]}
-    ) : (
-      c.action.setIcon({path: "on.png"}),
-      {enableRulesetIds: ["0"]}
-    )
+    (await c.storage.local.get("0"))[0] ?
+      (c.action.setIcon({path: "off.png"}), {disableRulesetIds:["0"]}) :
+      (c.action.setIcon({path: "on.png"}), {enableRulesetIds: ["0"]})
   ),
   c.action.onClicked.addListener(async ()=>
     c.declarativeNetRequest.updateEnabledRulesets(
-      (await c.storage.local.get("0"))[0] ? (
-        c.action.setIcon({path: "on.png"}),
-        c.storage.local.set({0:0}),
-        {enableRulesetIds: ["0"]}
-      ) : (
-        c.action.setIcon({path: "off.png"}),
-        c.storage.local.set({0:1}),
-        {disableRulesetIds: ["0"]}
-      )        
+      (await c.storage.local.get("0"))[0] ?
+        (c.action.setIcon({path: "on.png"}), c.storage.local.set({0:0}), {enableRulesetIds: ["0"]}) :
+        (c.action.setIcon({path: "off.png"}), c.storage.local.set({0:1}), {disableRulesetIds: ["0"]})        
     )
   )
 ))(chrome)
