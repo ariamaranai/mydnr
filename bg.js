9d283d426124d0d091e86ce683e91a0e91fef3d7
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-12-25 11:04:48 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -5,3 +5,8 @@ chrome.action.onClicked.addListener(async () =>
       : (chrome.action.setIcon({ path: "on.png" }), { enableRulesetIds: ["0"] })
   )
 );
+chrome.management.onEnabled.addListener(async info =>
+  info.id == chrome.runtime.id &&
+  (await chrome.declarativeNetRequest.getEnabledRulesets()).length ||
+  chrome.action.setIcon({ path: "off.png" })
+);
