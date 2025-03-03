1565c26ff75d2c8346b07afc53528d373dbf49a5
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-03-03 23:37:44 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,10 +1,12 @@
-chrome.declarativeNetRequest.getEnabledRulesets(rulesetIds => {
-  chrome.action.setIcon({ path: rulesetIds.length ? "on.png" : "off.png" });
-});
-chrome.action.onClicked.addListener(async () => {
-  let len = (await chrome.declarativeNetRequest.getEnabledRulesets()).length;
-  chrome.action.setIcon({ path: len ? "off.png": "on.png" }),
-  chrome.declarativeNetRequest.updateEnabledRulesets({
-    [len ? "disableRulesetIds" : "enableRulesetIds"] : ["0"]
+(async () => {
+  await chrome.action.setIcon({ path:
+    (await chrome.declarativeNetRequest.getEnabledRulesets()).length ? "on.png" : "off.png"
   });
-});
+  chrome.action.onClicked.addListener(async () => {
+    let len = (await chrome.declarativeNetRequest.getEnabledRulesets()).length;
+    await chrome.action.setIcon({ path: len ? "off.png": "on.png" }),
+    await chrome.declarativeNetRequest.updateEnabledRulesets({
+      [len ? "disableRulesetIds" : "enableRulesetIds"] : ["0"]
+    });
+  });
+})()
